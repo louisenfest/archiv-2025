68784615b8b78e7e679776e86932f3fc678a71c5
louisenfest <jeryspam3@web.de>
2025-06-22 22:49:11 +0200

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("content/media");
+
+  eleventyConfig.addCollection("mediaFiles", function(collectionApi) {
+    const mediaDir = "content/media";
+    return fs.readdirSync(mediaDir).map(name => {
+      return {
+        url: `/media/${name}`,
+        name
+      };
+    });
+  });
+
+  return {
+    dir: { input: "src", output: "_site" },
+  };
+};
