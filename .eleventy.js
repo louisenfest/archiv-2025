422609ac8ff321da737595e47fb6c6046039d119
louisenfest <jeryspam3@web.de>
2025-06-22 23:36:45 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,15 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "content/media": "media" });
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("content/media");
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
 
   eleventyConfig.addCollection("mediaFiles", function(collectionApi) {
     const mediaDir = "content/media";
